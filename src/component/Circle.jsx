83c54227e3f1681e1d78f933d03dcd087e58e011
ben <gamershubx902@gmail.com>
2025-04-12 14:20:26 +0100

    Hero Page

--- src/component/Circle.jsx
+++ src/component/Circle.jsx
@@ -1,46 +1,64 @@
 import React from 'react';
 import './Circle.css';
-import Traveler from './images/Traveler.png'
-import Book from './images/Book.jpg'
-import ReadingMAn from './images/ReadingMan.png'
+import Traveler from './images/Traveler.png';
+import Book from './images/Book.jpg';
+import ReadingMan from './images/ReadingMan.png';
 
 const Circle = () => {
-    return (
-      <div className="circle">
-        
-        {/* Add 4 images with different rotation angles */}
-        <div className="inner-image-green l1" style={{ '--angle': '343deg' }}>
-        <img className='image-inner' src={Traveler} alt="" />
-        <div className='image-green'>
-        wildlands
+  const items = [
+    {
+      src: Traveler,
+      alt: 'wildlands',
+      label: 'wildlands',
+      className: 'inner-image-green',
+      angle: '343deg',
+    },
+    {
+      src: Traveler,
+      alt: 'wildlands testnet',
+      label: 'wildlands testnet',
+      className: 'inner-image-green',
+      angle: '54deg',
+    },
+    {
+      src: Traveler,
+      alt: 'stronghold testnet',
+      label: 'stronghold testnet',
+      className: 'inner-image',
+      angle: '129deg',
+    },
+    {
+      src: ReadingMan,
+      alt: 'STRONGHOLD',
+      label: 'STRONGHOLD',
+      className: 'inner-image',
+      angle: '200deg',
+    },
+    {
+      src: Book,
+      alt: 'Documentation',
+      label: 'Documentation',
+      className: 'inner-image',
+      angle: '270deg',
+    },
+  ];
+
+  return (
+    <div className="circle">
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className={`${item.className} l${index + 1}`}
+          style={{ '--angle': item.angle }}
+        >
+          <img className="image-inner" src={item.src} alt={item.alt} />
+          <div className={item.className === 'inner-image-green' ? 'image-green' : item.className === 'inner-image' && 'image-word'}>
+            {item.label}
+          </div>
         </div>
-        </div>
-        <div className="inner-image-green l2" style={{ '--angle': '54deg' }}>
-        <img className='image-inner' src={Traveler} alt="" />
-        <div className='image-green'>
-        wildlands testnet
-        </div>
-        </div>
-        <div className="inner-image l3" style={{ '--angle': '129deg' }}>
-        <img className='image-inner' src={Traveler} alt="" />
-        <div className='image-word'>
-        stronghold testnet
-        </div>
-        </div>
-        <div className="inner-image l4" style={{ '--angle': '200deg' }}>
-        <img className='image-inner' src={ReadingMAn} alt="" />
-        <div className='image-word'>
-        STRONGHOLD
-        </div>
-        </div>
-        <div className="inner-image l5" style={{ '--angle': '270deg' }}>
-        <img className='image-inner' src={Book} alt="" />
-        <div className='image-mix'>
-          Documentation
-        </div>
-        </div>
-      </div>
-    );
-  };
+      ))}
+    </div>
+  );
+};
 
 export default Circle;
