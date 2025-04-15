e5a2dfba4465797dc3fe752c475f7f6e41610ead
ben <gamershubx902@gmail.com>
2025-04-15 20:46:24 +0100

    Updated page

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -12,6 +12,7 @@ return (
                 <a href="https://cdn.discordapp.com/attachments/1264339140724392007/1358379059838713940/BetterBank_whitepaper_1.7.pdf?ex=67fd83d5&is=67fc3255&hm=4f66398febaaa45cafd9dec61ad735fe3309d0617aa6bf3fd46e8e70e3ca3c5f&" className="footer-link">Whitepaper</a>
                 <a href="https://discord.gg/zPzF29vUrn" target="_blank" rel="noopener noreferrer" className="footer-link">Discord</a>
                 <a href="https://x.com/BetterBank_io" target="_blank" rel="noopener noreferrer" className="footer-link">Twitter</a>
+                <a href="https://t.me/better_bank" target="_blank" rel="noopener noreferrer" className="footer-link">Telegram</a>
             </div>
         </div>
     </footer>
