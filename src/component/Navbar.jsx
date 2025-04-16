8e65262cf73a0b8874b8581d066ebfff38d12d9f
ben <gamershubx902@gmail.com>
2025-04-16 22:45:50 +0100

    Updated page

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -16,8 +16,9 @@ const Navbar = () => {
   <a className="nav-link" href="https://betterbank.io/">Home</a>
   <a className="nav-link" href="https://presale.betterbank.io/presale/">Presale</a>
   <a className="nav-link" href="https://testnet.betterbank.io/">Testnet</a>
-  <a className="nav-link" href="https://share.proto.io/SF6XXG/fullscreen/">Test page</a>
+  <a className="nav-link" href="https://share.proto.io/SF6XXG/fullscreen/">UI showcase</a>
   <a className="nav-link" href="https://t.me/better_bank">Telegram</a>
+  <a className="nav-link" href="https://discord.gg/zPzF29vUrn">discord</a>
 </div>
 
       {/* App Button*/}
