e0172433a653399e8257209ed52f17ebacd914d9
ben <gamershubx902@gmail.com>
2025-04-17 17:45:13 +0100

    Merge remote changes

--- src/component/Tree.jsx
+++ src/component/Tree.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Tree from './images/tree.svg';
 import Logo from './images/Logo.svg';
 import './Tree.css';
 import Circle from './Circle.jsx'
 const TreeRender = () => {
 return (
     <div className="tree-container">
-            <div src= '' alt="Tree" className="tree-image">
+            <div className="tree-image">
                 <Circle/>
                 <img src={Logo} alt="Logo" className="logo-image"  onContextMenu={(e) => e.preventDefault()} draggable="false" />
             </div>
