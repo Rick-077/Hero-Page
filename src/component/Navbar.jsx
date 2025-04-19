761146bbd7cc1b5094ec0b84b893c67a5940f4f9
ben <gamershubx902@gmail.com>
2025-04-19 01:08:39 +0100

    updated banner

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import slogo from './images/Logo.svg';
-import Banner from  './images/banner.png';
+import Banner1 from  './images/banner-desktop.svg';
+import Banner2 from  './images/banner.svg';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
 <div className='nav'>
-<a href="https://presale.betterbank.io/presale/"><img className='banner' src={Banner}alt="" /></a>
+<a href="https://presale.betterbank.io/presale/">
+<picture>
+    <source media="(max-width: 768px)" srcSet= {Banner2} />
+    <source media="(min-width: 769px)" srcSet= {Banner1} />
+    <img className='banner' src= {Banner1} alt="" />
+  </picture>
+</a>
     <nav className="navbar-container">
       {/* Logo */}
         <div className="logo-icon">
